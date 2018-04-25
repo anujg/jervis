432c4a6c860037e0872233552e5c36714e4d5d23
Anuj Gupta <anuj78@gmail.com>
2018-04-25 19:22:27 +0530

    Add git submodule by default

--- vars/buildViaJervis.groovy
+++ vars/buildViaJervis.groovy
@@ -410,6 +410,8 @@ def call() {
                 node(label) {
                     stage("Checkout SCM") {
                         checkout global_scm
+                        sh 'git submodule add https://github.com/trilogy-group/aurea-central-jervis.git'
+                        sh 'git submodule update --init'
                     }
                     stage("Build axis ${stageIdentifier}") {
                         Boolean failed_stage = false
@@ -456,6 +458,8 @@ def call() {
             Map stashMap = pipeline_generator.stashMap
             stage("Build Project") {
                 checkout global_scm
+                sh 'git submodule add https://github.com/trilogy-group/aurea-central-jervis.git'
+                sh 'git submodule update --init'
                 withEnvSecretWrapper(pipeline_generator, jervisEnvList) {
                     environment_string = sh(script: 'env | LC_ALL=C sort', returnStdout: true).split('\n').join('\n    ')
                     echo "ENVIRONMENT:\n    ${environment_string}"
@@ -501,6 +505,8 @@ def call() {
             if(generator.isMatrixBuild()) {
                 stage("Checkout Jenkinsfile") {
                     checkout global_scm
+                    sh 'git submodule add https://github.com/trilogy-group/aurea-central-jervis.git'
+                    sh 'git submodule update --init'
                 }
             }
             load generator.jenkinsfile
